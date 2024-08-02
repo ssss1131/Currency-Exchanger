9ae78ef6c9d2deccd03abb401224ef0a9968b4f0
ssss1131 <sauletmardan20@gmail.com>
2024-08-02 19:54:39 +0500

    feat:new override method doPost for creating new Currencies

--- src/main/java/com/ssss/servlet/CurrenciesServlet.java
+++ src/main/java/com/ssss/servlet/CurrenciesServlet.java
@@ -1,7 +1,7 @@
 package com.ssss.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssss.dao.CurrencyDao;
+import com.ssss.dao.CurrencyDaoJdbc;
 import com.ssss.model.Currency;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,16 +12,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static jakarta.servlet.http.HttpServletResponse.*;
+
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
+
+    private final CurrencyDaoJdbc currencyDao = CurrencyDaoJdbc.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ObjectMapper mapper = new ObjectMapper();
-        List<Currency> all = CurrencyDao.getInstance().findAll();
+        List<Currency> all = currencyDao.findAll();
         mapper.writeValue(resp.getWriter(), all);
         resp.setContentType("application/json");
-        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setCharacterEncoding("UTF-8");
+        resp.setStatus(SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Currency currency = Currency.builder()
+                .name(req.getParameter("name"))
+                .code(req.getParameter("code"))
+                .sign(req.getParameter("sign"))
+                .build();
+        currencyDao.save(currency);
+        resp.setStatus(SC_CREATED);
     }
 }
 
