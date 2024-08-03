3e8b5be64aed800337b2516640afd1f9c2d8b4a5
ssss1131 <sauletmardan20@gmail.com>
2024-08-03 21:14:21 +0500

    feat:new method for updating exchange rate and some refactor in method findByCodes(still needs refactor)

--- src/main/java/com/ssss/dao/ExchangeRateDaoJdbc.java
+++ src/main/java/com/ssss/dao/ExchangeRateDaoJdbc.java
@@ -4,6 +4,7 @@ import com.ssss.model.Currency;
 import com.ssss.model.ExchangeRate;
 import com.ssss.util.ConnectionManager;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -44,7 +45,14 @@ public class ExchangeRateDaoJdbc implements Dao<ExchangeRate> {
                                         BaseCurrencyId,
                                         TargetCurrencyId,
                                         Rate
-            ) VALUES (?, ?, ?);
+                                    ) VALUES (?, ?, ?);
+            """;
+
+    private static final String UPDATE_SQL = """
+                                    UPDATE ExchangeRates
+                                    SET Rate =?
+                                    WHERE BaseCurrencyId=(SELECT ID FROM Currencies WHERE Code=?)
+                                    AND TargetCurrencyId=(SELECT ID FROM Currencies WHERE Code=?);
             """;
 
 
@@ -75,20 +83,45 @@ public class ExchangeRateDaoJdbc implements Dao<ExchangeRate> {
 
     @Override
     public Optional<ExchangeRate> save(ExchangeRate entity) {
-        return null;
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SAVE_SQL)) {
+            statement.setInt(1, entity.getBaseCurrency().getId());
+            statement.setInt(2, entity.getTargetCurrency().getId());
+            statement.setBigDecimal(3, entity.getRate());
+            statement.executeUpdate();
+            return findByCodes(entity.getBaseCurrency().getCode(), entity.getTargetCurrency().getCode());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Optional<ExchangeRate> findByCode(String baseCode, String targetCode) {
+    public Optional<ExchangeRate> findByCodes(String baseCode, String targetCode) {
         try (Connection connection = ConnectionManager.getConnection();
              PreparedStatement statement = connection.prepareStatement(FIND_BY_CODES)) {
             statement.setString(1, baseCode);
             statement.setString(2, targetCode);
             ResultSet resultSet = statement.executeQuery();
-            // TODO maybe exception if resultset null bcs no such base-target codes
-            resultSet.next();
-            return Optional.of(buildExchangeRate(resultSet));
+            if (resultSet.next()) {
+                return Optional.of(buildExchangeRate(resultSet));
+            } else {
+                return Optional.empty();
+            }
 
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<ExchangeRate> updateByCodes(String baseCode, String targetCode, BigDecimal newRate) {
+        try(Connection connection = ConnectionManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(UPDATE_SQL)) {
+            statement.setBigDecimal(1, newRate);
+            statement.setString(2, baseCode);
+            statement.setString(3, targetCode);
+            statement.executeUpdate();
+            return findByCodes(baseCode, targetCode);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
