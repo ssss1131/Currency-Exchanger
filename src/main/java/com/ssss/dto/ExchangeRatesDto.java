5e38bcf655e26ee55594d7e91aa42e27ec2e9ebc
ssss1131 <sauletmardan20@gmail.com>
2024-08-03 21:09:22 +0500

    feat: new dto for transfer information from servlet to service

---
+++ src/main/java/com/ssss/dto/ExchangeRatesDto.java
@@ -0,0 +1,14 @@
+package com.ssss.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
+@Builder
+public class ExchangeRatesDto {
+    String baseCurrencyCode;
+    String targetCurrencyCode;
+    BigDecimal rate;
+}
