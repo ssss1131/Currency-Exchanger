e6dc4d340d2069b0bcc20f2065558810cfbab33b
ssss1131 <sauletmardan20@gmail.com>
2024-08-03 21:15:03 +0500

    refactor: doPost method is more accurate but still not enough

--- src/main/java/com/ssss/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/ssss/servlet/ExchangeRatesServlet.java
@@ -2,6 +2,9 @@ package com.ssss.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssss.dao.ExchangeRateDaoJdbc;
+import com.ssss.dto.ExchangeRatesDto;
+import com.ssss.model.ExchangeRate;
+import com.ssss.service.ExchangeRatesService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import static jakarta.servlet.http.HttpServletResponse.*;
 
@@ -16,24 +20,34 @@ import static jakarta.servlet.http.HttpServletResponse.*;
 public class ExchangeRatesServlet extends HttpServlet {
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private final ExchangeRateDaoJdbc exchangeRate  = ExchangeRateDaoJdbc.getInstance();
+    private final ExchangeRatesService exchangeRatesService = ExchangeRatesService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        mapper.writeValue(resp.getWriter(), exchangeRate.findAll());
+        mapper.writeValue(resp.getWriter(), ExchangeRateDaoJdbc.getInstance().findAll());
         resp.setStatus(SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO need to save but form give me Id not a Currency so i have like 2 options
-        // 1.Create full new Currency and then in save method get its id
-        // 2.Create Currency with only id field
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-
-
+        //TODO filter or validator bcs there is Integer.valueOf mb exception
+        ExchangeRatesDto dto = ExchangeRatesDto.builder()
+                .baseCurrencyCode(req.getParameter("baseCurrencyCode"))
+                .targetCurrencyCode(req.getParameter(("targetCurrencyCode")))
+                .rate(new BigDecimal(req.getParameter("rate")))
+                .build();
+        try{
+            ExchangeRate exchangeRate = exchangeRatesService.createExchangeRate(dto);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(SC_CREATED);
+            mapper.writeValue(resp.getWriter(), exchangeRate);
+            System.out.println(exchangeRate);
+        }catch(Exception e) {
+            //TODO catch all possible exceptions
+            resp.sendError(SC_BAD_REQUEST, "something короч не знаю потом сделаю лень щя");
+        }
     }
 }
