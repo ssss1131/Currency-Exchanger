2e7162c54b5c7ed6aa913c22e409256e94e35d19
ssss1131 <sauletmardan20@gmail.com>
2024-08-06 21:30:11 +0500

    refactor: now exceptions handle on dao

--- src/main/java/com/ssss/service/CurrencyService.java
+++ src/main/java/com/ssss/service/CurrencyService.java
@@ -1,15 +1,15 @@
 package com.ssss.service;
 
+import com.ssss.dao.CurrencyDao;
 import com.ssss.dao.CurrencyDaoJdbc;
 import com.ssss.model.Currency;
 
 import java.util.List;
-import java.util.Optional;
 
 public class CurrencyService {
 
     private static final CurrencyService INSTANCE = new CurrencyService();
-    private final CurrencyDaoJdbc currencyDaoJdbc = CurrencyDaoJdbc.getInstance();
+    private final CurrencyDao currencyDaoJdbc = CurrencyDaoJdbc.getInstance();
 
     public static CurrencyService getInstance() {
         return INSTANCE;
@@ -19,8 +19,7 @@ public class CurrencyService {
     }
 
     public Currency getCurrencyByCode(String currencyCode) {
-        return currencyDaoJdbc.findByCode(currencyCode)
-                .orElseThrow( () -> new IllegalArgumentException("Currency not found"));
+        return currencyDaoJdbc.findByCode(currencyCode);
     }
 
     public List<Currency> getAllCurrencies() {
@@ -28,8 +27,7 @@ public class CurrencyService {
     }
 
     public Currency save(Currency currency) {
-        return currencyDaoJdbc.save(currency)
-                .orElseThrow(() -> new IllegalArgumentException("Currency already exists"));
+        return currencyDaoJdbc.save(currency);
     }
 
 }
