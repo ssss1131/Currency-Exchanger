e69d036bb9a802bdb6838e13ee7fac830013c9e2
ssss1131 <sauletmardan20@gmail.com>
2024-08-06 20:59:23 +0500

    refactor: delete some unnecessary things

--- src/main/java/com/ssss/exception/CurrencyConversionException.java
+++
@@ -1,7 +0,0 @@
-package com.ssss.exception;
-
-public class CurrencyConversionException extends RuntimeException {
-    public CurrencyConversionException(String message) {
-        super(message);
-    }
-}
